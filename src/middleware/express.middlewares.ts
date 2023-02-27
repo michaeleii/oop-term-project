8eb44bcfb872c75fa5b55c78903d57079830116d
michaellei-e0ca <michaelleie0ca@gmail.com>
2023-02-27 12:17:30 -0800

    Connected to redis database

--- src/middleware/express.middlewares.ts
+++ src/middleware/express.middlewares.ts
@@ -2,6 +2,21 @@ import express from "express";
 import path from "path";
 import session from "express-session";
 import morgan from "morgan";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+let RedisStore = require("connect-redis")(session);
+const Redis = require("ioredis");
+
+const REDIS_PASSWORD = process.env.REDIS_PASSWORD as string;
+const REDIS_HOST = process.env.REDIS_HOST as string;
+const REDIS_PORT = process.env.REDIS_PORT as string;
+
+let redisClient = new Redis({
+  port: REDIS_PORT, // Redis port
+  host: REDIS_HOST, // Redis host
+  password: REDIS_PASSWORD,
+});
 
 module.exports = (app) => {
   // Static File Serving and Post Body Parsing
@@ -16,6 +31,7 @@ module.exports = (app) => {
   // Session Configuration
   app.use(
     session({
+      store: new RedisStore({ client: redisClient }),
       secret: "secret",
       resave: false,
       saveUninitialized: false,
