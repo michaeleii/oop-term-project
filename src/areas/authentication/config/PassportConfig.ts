6251f671065c7411b93b452fa441c07be4bde052
michaellei-e0ca <michaelleie0ca@gmail.com>
2023-02-27 12:41:31 -0800

    Added some passport stuff

--- src/areas/authentication/config/PassportConfig.ts
+++ src/areas/authentication/config/PassportConfig.ts
@@ -3,4 +3,27 @@
 //----------------------------------------
 // 🚀 Configure Passport.js Local Authentication in this file
 //    Ensure code is fully typed wherever possible (unless inference can be made)
-export default class PassportConfig {}
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+export default class PassportConfig {
+  private name: string;
+  private strategy: LocalStrategy;
+  constructor(name: string, strategy: LocalStrategy) {
+    this.name = name;
+    this.strategy = strategy;
+  }
+  private configure() {}
+  private serializeUser() {}
+  private deserializeUser() {}
+}
+
+const localStrategy = new LocalStrategy(
+  {
+    usernameField: "email",
+    passwordField: "password",
+  },
+  (email, password, done) => {
+    try {
+    } catch (error: any) {}
+  }
+);
