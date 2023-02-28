7d1a5f83211e5ae44f162ea718a2db8a004b4d90
Michael Lei <michaelleie0ca@gmail.com>
2023-02-28 12:00:33 -0800

    Add appendFile to append errors to the log

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,14 +1,16 @@
 import { NextFunction, Request, Response } from "express";
 import HttpException from "../exceptions/HttpException";
 import { appendFile } from "fs/promises";
+import DateFormatter from "../helper/DateFormatter";
 
-const errorMiddleware = (error: HttpException, request: Request, response: Response, next: NextFunction) => {
+const errorMiddleware = async (error: HttpException, request: Request, response: Response, next: NextFunction) => {
   const status = error.status || 500;
   const message = error.message || "An error has occured";
 
-  const currentDate = new Date(Date.now());
-
-  console.log(status, message);
+  const currentDate = DateFormatter.format(new Date(Date.now()));
+  const errorLog = `${currentDate} - ${status} - ${message}`;
+  await appendFile("../logs/error.log", errorLog);
+  next();
 };
 
 export default errorMiddleware;
