28fedd7ab471ebe138121ab81fef9b68a68853e2
Michael Lei <michaelleie0ca@gmail.com>
2023-02-28 12:36:12 -0800

    Finish errorMiddleWare

---
+++ src/helper/pathJoiner.ts
@@ -0,0 +1,7 @@
+import { join } from "path";
+export default class PathJoiner {
+  private static projectDir: string = __dirname;
+  static get errorLog(): string {
+    return join(this.projectDir, "..", "logs", "error.log");
+  }
+}
--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import HttpException from "../exceptions/HttpException";
 import { appendFile } from "fs/promises";
 import DateFormatter from "../helper/DateFormatter";
+import Path from "../helper/pathJoiner";
 
 const errorMiddleware = async (error: HttpException, request: Request, response: Response, next: NextFunction) => {
   const status = error.status || 500;
@@ -9,7 +10,8 @@ const errorMiddleware = async (error: HttpException, request: Request, response:
 
   const currentDate = DateFormatter.format(new Date(Date.now()));
   const errorLog = `${currentDate} - ${status} - ${message}`;
-  await appendFile("../logs/error.log", errorLog);
+  console.log(status, message);
+  await appendFile(Path.errorLog, errorLog);
   next();
 };
 
