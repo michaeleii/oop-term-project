6fc89938cb6205aa7b109ff4c5c1e17a5b9640d6
Michael Lei <michaelleie0ca@gmail.com>
2023-03-01 01:35:39 -0800

    Add likePostById function to like and dislike posts

--- src/areas/post/controllers/post.controller.ts
+++ src/areas/post/controllers/post.controller.ts
@@ -20,6 +20,7 @@ class PostController implements IController {
     this.router.get(`${this.path}/:id`, this.getPostById);
     this.router.get(`${this.path}/:id/delete`, this.deletePost);
     this.router.post(`${this.path}/:id/comment`, this.createComment);
+    this.router.post(`${this.path}/:id/like`, this.likePostById);
     this.router.post(`${this.path}`, this.createPost);
   }
 
@@ -32,15 +33,29 @@ class PostController implements IController {
   };
 
   // 🚀 This method should use your postService and pull from your actual fakeDB, not the temporary post object
-  private getPostById = async (request: Request, res: Response, next: NextFunction) => {
-    const id = request.params.id;
-    const post = this.postService.findById(parseInt(id));
+  private getPostById = async (req: Request, res: Response, next: NextFunction) => {
+    const id = +req.params.id;
+    const post = this.postService.findById(id);
     const postFormatted = new PostViewModel(post);
     res.render("post/views/post", { post: postFormatted });
   };
 
+  private likePostById = async (req: Request, res: Response, next: NextFunction) => {
+    const user = await req.user;
+    const postId = +req.params.id;
+    const post = new PostViewModel(this.postService.findById(postId));
+    const likedPost = post.likes.find((like) => like.userId === user.id);
+    if (likedPost) {
+      this.postService.unlikePost(postId, user.id);
+    } else {
+      this.postService.likePost(postId, user.id);
+    }
+    res.redirect("back");
+  };
+
   // 🚀 These post methods needs to be implemented by you
   private createComment = async (req: Request, res: Response, next: NextFunction) => {};
+
   private createPost = async (req: Request, res: Response, next: NextFunction) => {
     const user = await req.user;
     const message: string = req.body.postText;
