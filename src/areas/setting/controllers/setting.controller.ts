0e88cd08661dcff8cf25a5ff9eb6a3782f1892a7
Michael Lei <michaelleie0ca@gmail.com>
2023-03-01 09:08:48 -0800

    Setup settings

--- src/areas/setting/controllers/setting.controller.ts
+++ src/areas/setting/controllers/setting.controller.ts
@@ -6,29 +6,40 @@ import { ensureAuthenticated } from "../../../middleware/authentication.middlewa
 class SettingController implements IController {
   public path = "/setting";
   public router = Router();
+  settingService: ISettingService;
 
   constructor(settingService: ISettingService) {
+    this.settingService = settingService;
     this.initializeRoutes();
   }
 
   private initializeRoutes() {
     this.router.get(`${this.path}/`, ensureAuthenticated, this.getSettingsPage);
-    //Route to change username
-    //Route to change email
-    //Route to change password
+    this.router.post(`Route to change username`, ensureAuthenticated, this.changeUsername);
+    this.router.post(`Route to change email`, ensureAuthenticated, this.changeEmail);
+    this.router.post(`Route to change password`, ensureAuthenticated, this.changePassword);
   }
   private getSettingsPage = async (request: Request, res: Response, next: NextFunction) => {
     const user = await request.user;
     res.render("setting/views/setting", { user });
   };
-  private changeUsername = async (request: Request, res: Response, next: NextFunction) => {
-    const user = await request.user;
+  private changeUsername = async (req: Request, res: Response, next: NextFunction) => {
+    const user = await req.user;
+    console.log(req.body);
+    // this.settingService.changeUsername();
+    res.redirect("/setting");
   };
-  private changeEmail = async (request: Request, res: Response, next: NextFunction) => {
-    const user = await request.user;
+  private changeEmail = async (req: Request, res: Response, next: NextFunction) => {
+    const user = await req.user;
+    console.log(req.body);
+    // this.settingService.changeEmail();
+    res.redirect("/setting");
   };
-  private changePassword = async (request: Request, res: Response, next: NextFunction) => {
-    const user = await request.user;
+  private changePassword = async (req: Request, res: Response, next: NextFunction) => {
+    const user = await req.user;
+    console.log(req.body);
+    // this.settingService.changePassword();
+    res.redirect("/setting");
   };
 }
 
