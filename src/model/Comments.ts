a9381f7a98f82b98bdfc92b6df3629220f0e3386
Michael Lei <michaelleie0ca@gmail.com>
2023-02-28 17:31:10 -0800

    Update comments fake data

--- src/model/Comments.ts
+++ src/model/Comments.ts
@@ -1,34 +1,22 @@
-interface Comment {
-      id: string;
-      message: string;
-      commentCreatorId: string;
-      postId: string;
-      createdAt: Date;
-    }
-    
-    const comments: Comment[] = [
-      {
-        id: "abc2",
-        message: "this is some random comment",
-        commentCreatorId: "billgates",
-        postId: "abc1",
-        createdAt: new Date("2012-01-09T11:25:13Z"),
-      },
-      {
-        id: "abc4",
-        message: "Cool post james. Glad I decided to follow you.",
-        commentCreatorId: "billgates",
-        postId: "abc3",
-        createdAt: new Date("2012-01-05T04:13:24Z"),
-      },
-      {
-        id: "abc6",
-        message: "The weather is always nice when you're rich like me.",
-        commentCreatorId: "billgates",
-        postId: "abc5",
-        createdAt: new Date("2012-02-05T05:13:24Z"),
-      },
-    ];
-    
-    export { Comment, comments };
-    
+import IComment from "../interfaces/comment.interface";
+
+const comments: IComment[] = [
+  {
+    id: "abc2",
+    createdAt: "2012-01-09T11:25:13Z",
+    userId: "billgates",
+    message: "this is some random comment",
+  },
+  {
+    id: "abc4",
+    createdAt: "2012-01-05T04:13:24Z",
+    userId: "billgates",
+    message: "Cool post james. Glad I decided to follow you.",
+  },
+  {
+    id: "abc6",
+    userId: "billgates",
+    createdAt: "2012-02-05T05:13:24Z",
+    message: "The weather is always nice when you're rich like me.",
+  },
+];
