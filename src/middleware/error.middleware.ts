52b28f5702b3c3b640f61163873734644b6889a6
Michael Lei <michaelleie0ca@gmail.com>
2023-03-01 13:37:04 -0800

    Shorter parameter names

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -2,12 +2,12 @@ import { NextFunction, Request, Response } from "express";
 import HttpException from "../exceptions/HttpException";
 import { appendFile } from "fs/promises";
 import DateFormatter from "../helper/DateFormatter";
-import Path from "../helper/pathJoiner";
+import Path from "../helper/PathJoiner";
 import { EOL } from "os";
 
-const errorMiddleware = async (error: HttpException, request: Request, response: Response, next: NextFunction) => {
-  const status = error.status || 500;
-  const message = error.message || "An error has occured";
+const errorMiddleware = async (err: HttpException, req: Request, res: Response, next: NextFunction) => {
+  const status = err.status || 500;
+  const message = err.message || "An error has occured";
 
   const currentDate = DateFormatter.format(new Date(Date.now()));
   const errorLog = `${currentDate} - ${status} - ${message} ${EOL}`;
