c25f589240e315c81918f6b6bbdb456141519710
Michael Lei <michaelleie0ca@gmail.com>
2023-02-28 17:32:26 -0800

    Add export

--- src/model/Comments.ts
+++ src/model/Comments.ts
@@ -1,20 +1,20 @@
 import IComment from "../interfaces/comment.interface";
 
-const comments: IComment[] = [
+export const comments: IComment[] = [
   {
-    id: "abc2",
+    id: "1",
     createdAt: "2012-01-09T11:25:13Z",
     userId: "billgates",
     message: "this is some random comment",
   },
   {
-    id: "abc4",
+    id: "2",
     createdAt: "2012-01-05T04:13:24Z",
     userId: "billgates",
     message: "Cool post james. Glad I decided to follow you.",
   },
   {
-    id: "abc6",
+    id: "3",
     userId: "billgates",
     createdAt: "2012-02-05T05:13:24Z",
     message: "The weather is always nice when you're rich like me.",
