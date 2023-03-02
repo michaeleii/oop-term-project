ecdfef355fe02786c39460bca5243bdd8b1c4c3f
Michael Lei <michaelleie0ca@gmail.com>
2023-03-01 17:20:07 -0800

    Remove comments

--- src/areas/post/views/post.viewmodel.ts
+++ src/areas/post/views/post.viewmodel.ts
@@ -4,18 +4,6 @@ import IPost from "../../../interfaces/post.interface";
 import ILike from "../../../interfaces/like.interface";
 import { CommentViewModel } from "../comment.viewmodel";
 
-// The following is an (incomplete) example of what a view model may look like
-// The purpose of a view model is to format the incoming data from the database
-// into what the ejs page requires specifically.
-
-// For example, you may need to show a date on the ejs page like so:
-// Date: Monday, Jan 14, 2021
-
-// The default date format in javascript when you call new Date() looks completely different.
-// You could solve this in the view model by changing line 19 to a string, and then having a helper
-// method which converts the incoming date from the constructor (of type date) to a string formatted
-// date that you store in createdAt.
-
 export class PostViewModel {
   readonly _db = database;
   public id: number;
