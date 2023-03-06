ff41287f1eac5d396f05e3a23f179aac5237ab4b
Michael Lei <michaelleie0ca@gmail.com>
2023-03-05 22:00:21 -0800

    Finish change username function

--- src/areas/setting/services/Setting.service.mock.ts
+++ src/areas/setting/services/Setting.service.mock.ts
@@ -4,12 +4,9 @@ import ISettingService from "../services/ISettingService";
 export class MockSettingService implements ISettingService {
   readonly _db = database;
   changeUsername(userId: number, username: string): void {
-    throw new Error("Method not implemented.");
+    const user = this._db.users.find((user) => user.id === userId);
+    user.username = username;
   }
-  changeEmail(userId: number, email: string): void {
-    throw new Error("Method not implemented.");
-  }
-  changePassword(userId: number, currentPassword: string, newPassword: string): void {
-    throw new Error("Method not implemented.");
-  }
+  changeEmail(userId: number, email: string): void {}
+  changePassword(userId: number, currentPassword: string, newPassword: string): void {}
 }
